8b0373b4aa5218d8f365b376ed54cf84dd734451
Daban Sadiq <dabansadiq@icloud.com>
2021-04-14 12:59:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+  const par = document.getElementById('text')
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+
+  par.innerHTML ="This is really cool!"
+});
