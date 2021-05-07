29124172eeb32404c5a97dc9c210018b1bcb0a2f
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-05-07 12:29:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+    const textPart = document.getElementById("text");
+    
+    document.addEventListener("DOMContentLoaded", function() {
+      textPart.innerHTML = "This is really cool!";
+    });
